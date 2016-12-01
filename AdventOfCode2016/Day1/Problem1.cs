d6e52ac1308b552bf8b9ab8209a01456723f4221
Chris Bye <chriswbye@gmail.com>
2016-12-01 09:38:02 -0500

    added structure

---
+++ AdventOfCode2016/Day1/Problem1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016.Day1
+{
+    using System.Net.NetworkInformation;
+
+    public class Problem1
+    {
+        public static object SolveProblem1()
+        {
+
+            return null;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Day1Prob1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2016.Day1;
+
+namespace AdventOfCodeTest
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class Day1Prob1
+    {
+        private ITestOutputHelper _output;
+
+        public Day1Prob1(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+        [Fact]
+        public  void RunSolution()
+        {
+            var result = Problem1.SolveProblem1();
+            _output.WriteLine((result ?? "null").ToString());
+            
+        }
+    }
+}
